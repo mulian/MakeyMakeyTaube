bcecd972528cb2eb4ff7ef79c037571090dd5bc1
mulian <Max.Hoffmann@tu-bs.de>
2016-06-15 11:01:23 +0200

    login#2

--- imports/ui/components/login/login.js
+++ imports/ui/components/login/login.js
@@ -4,8 +4,9 @@ Template.app_login.onRendered(function() {
 });
 
 Template.app_login.events({
-  'click #login-dropdown-list': function(e,i) {
+  'click #login-buttons': function(e,i) {
     setTimeout(() => {
+      console.log(location.host);
       if((window.location.host.indexOf('home.wi2.phil.tu-bs.de')>=0) &&
         (window.location.hash.indexOf('letmein')<0) &&
         (Meteor.userId()==null))
