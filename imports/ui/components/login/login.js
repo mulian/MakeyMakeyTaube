ad2bedce7379c17c0a92c64f3a38e2ad686613bf
mulian <Max.Hoffmann@tu-bs.de>
2016-06-15 12:23:56 +0200

    remove log

--- imports/ui/components/login/login.js
+++ imports/ui/components/login/login.js
@@ -6,7 +6,6 @@ Template.app_login.onRendered(function() {
 Template.app_login.events({
   'click #login-buttons': function(e,i) {
     setTimeout(() => {
-      console.log(location.host);
       if((window.location.host.indexOf('home.wi2.phil.tu-bs.de')>=0) &&
         (window.location.hash.indexOf('letmein')<0) &&
         (Meteor.userId()==null))
